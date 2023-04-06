416a90ff174d1f1ae09ed437a29e20223b026183
Gabriel <Boaventura-gabriel@hotmail.com>
2023-04-06 20:41:09 -0300

    Mudança Visual

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View, TouchableOpacity, Button } from 'react-native';
 
 export default function App() {
   // Mapeamento de teclas
-  const buttons = ['LIMPAR', 'DEL', '%', '/', 9, 8, 7, "x", 6, 5, 4, "-", 1, 2, 3,"+", 0,'.', '+/-', '=']
+  const buttons = ['LIMPAR', 'DEL', '%', '/', 7, 8, 9,"x", 4, 5, 6, "-", 1, 2, 3,"+", 0,'.', '+/-', '=']
 
   const [currentNumber, setCurrentNumber] = useState("")
   const [lastNumber, setLastNumber] = useState("")
@@ -73,12 +73,12 @@ export default function App() {
 
         {buttons.map((button) => 
           button === '=' ? // Mapeamento do botão =
-        <TouchableOpacity onPress={() => handleInput(button)} key={button} style={[styles.button, {backgroundColor: '#3dd0e3'}]}>
-          <Text style={[styles.textButton, {color: "white", fontSize: 50}]}>{button}</Text>
+        <TouchableOpacity onPress={() => handleInput(button)} key={button} style={[styles.button, {backgroundColor: '#2b124f'}]}>
+          <Text style={[styles.textButton, {color: "gray", fontSize: 50}]}>{button}</Text>
         </TouchableOpacity>
           : // Mapeamento dos outros botões
           <TouchableOpacity onPress={() => handleInput(button)} key={button} style={styles.button}>
-            <Text style={[styles.textButton, {color: typeof(button) === 'number' ? 'black': '#0093a6'}]}>{button}</Text>
+            <Text style={[styles.textButton, {color: typeof(button) === 'number' ? 'white': 'gray'}]}>{button}</Text>
           </TouchableOpacity>
         )}
       </View>
@@ -93,11 +93,11 @@ const styles = StyleSheet.create({
   results: {
     flex: 2,
     justifyContent: "center",
-    backgroundColor: "#f5f5f5"
+    backgroundColor: "#2b124f"
   },
   resultText: {
-    color: "#282F38",
-    fontSize: 32,
+    color: "white",
+    fontSize: 40,
     fontWeight: "bold",
     padding: 12,
     textAlign: "right"
@@ -113,15 +113,15 @@ const styles = StyleSheet.create({
     flexWrap: 'wrap',
   },
   button: {
-    backgroundColor: 'white',
+    backgroundColor: '#371566',
     alignItems: 'center',
     justifyContent: 'center',
     minWidth: 100, 
-    minHeight: 90,
-    flex: 2,
+    minHeight: 136,
+    flex:2,
   },
   textButton: {
-    color: "#7c7c7c",
-    fontSize: 20,
+    color: "#fffff",
+    fontSize: 25,
   } 
 });
