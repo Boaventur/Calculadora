ad0d0a240566bb5ad3806e12aeb55ab3c2f04778
Gabriel <Boaventura-gabriel@hotmail.com>
2023-04-06 21:34:46 -0300

    função inversão inserida

--- App.js
+++ App.js
@@ -1,57 +1,58 @@
 import React from 'react';
-import {useState} from 'react'
+import { useState } from 'react'
 import { StyleSheet, Text, View, TouchableOpacity, Button } from 'react-native';
 
 export default function App() {
   // Mapeamento de teclas
-  const buttons = ['LIMPAR', 'DEL', '%', '/', 7, 8, 9,"x", 4, 5, 6, "-", 1, 2, 3,"+", 0,'.', '+/-', '=']
+  const buttons = ['LIMPAR', 'DEL', '%', '/', 7, 8, 9, "x", 4, 5, 6, "-", 1, 2, 3, "+", 0, '.', '+/-', '=']
 
   const [currentNumber, setCurrentNumber] = useState("")
   const [lastNumber, setLastNumber] = useState("")
 
 
-  function calculator(){
+  function calculator() {
     const splitNumbers = currentNumber.split(' ')
     const fistNumber = parseFloat(splitNumbers[0])
     const lastNumber = parseFloat(splitNumbers[2])
     const operator = splitNumbers[1]
 
     // Faz ação referente tecla pressionada
-    switch(operator){
+    switch (operator) {
       case '+':
         setCurrentNumber((fistNumber + lastNumber).toString())
         return
-      case '-': 
+      case '-':
         setCurrentNumber((fistNumber - lastNumber).toString())
         return
       case 'x':
         setCurrentNumber((fistNumber * lastNumber).toString())
         return
-      case '/': 
-        setCurrentNumber((fistNumber /  lastNumber).toString())
+      case '/':
+        setCurrentNumber((fistNumber / lastNumber).toString())
         return
     }
   }
 
-  function handleInput(buttonPressed){
+  function handleInput(buttonPressed) {
     console.log(buttonPressed) // Mostra no Console a tecla pressionada
-    if(buttonPressed === '+' | buttonPressed === "-" | buttonPressed === "x" | buttonPressed === "/" ){
+    if (buttonPressed === '+' | buttonPressed === "-" | buttonPressed === "x" | buttonPressed === "/") {
       setCurrentNumber(currentNumber + " " + buttonPressed + " ")
       return
     }
-    switch(buttonPressed){
+    switch (buttonPressed) {
       case 'DEL':
         setCurrentNumber(currentNumber.substring(0, (currentNumber.length - 1)))
         return
       case 'LIMPAR': // Limpa todo o conteúdo
-        setLastNumber("") 
-        setCurrentNumber("") 
+        setLastNumber("")
+        
         return
       case '=':
         setLastNumber(currentNumber + " = ")
         calculator()
         return
       case '+/-':
+        setCurrentNumber((-1 * currentNumber).toString())
         return
     }
 
@@ -71,15 +72,15 @@ export default function App() {
       {/* Area onde os botões são exibidos*/}
       <View style={styles.buttons}>
 
-        {buttons.map((button) => 
+        {buttons.map((button) =>
           button === '=' ? // Mapeamento do botão =
-        <TouchableOpacity onPress={() => handleInput(button)} key={button} style={[styles.button, {backgroundColor: '#2b124f'}]}>
-          <Text style={[styles.textButton, {color: "gray", fontSize: 50}]}>{button}</Text>
-        </TouchableOpacity>
-          : // Mapeamento dos outros botões
-          <TouchableOpacity onPress={() => handleInput(button)} key={button} style={styles.button}>
-            <Text style={[styles.textButton, {color: typeof(button) === 'number' ? 'white': 'gray'}]}>{button}</Text>
-          </TouchableOpacity>
+            <TouchableOpacity onPress={() => handleInput(button)} key={button} style={[styles.button, { backgroundColor: '#2b124f' }]}>
+              <Text style={[styles.textButton, { color: "gray", fontSize: 50 }]}>{button}</Text>
+            </TouchableOpacity>
+            : // Mapeamento dos outros botões
+            <TouchableOpacity onPress={() => handleInput(button)} key={button} style={styles.button}>
+              <Text style={[styles.textButton, { color: typeof (button) === 'number' ? 'white' : 'gray' }]}>{button}</Text>
+            </TouchableOpacity>
         )}
       </View>
     </View>
@@ -102,7 +103,7 @@ const styles = StyleSheet.create({
     padding: 12,
     textAlign: "right"
   },
-  historyText:{
+  historyText: {
     color: "#7c7c7c",
     fontSize: 20,
     marginRight: 10,
@@ -116,12 +117,12 @@ const styles = StyleSheet.create({
     backgroundColor: '#371566',
     alignItems: 'center',
     justifyContent: 'center',
-    minWidth: 100, 
+    minWidth: 100,
     minHeight: 136,
-    flex:2,
+    flex: 2,
   },
   textButton: {
     color: "#fffff",
     fontSize: 25,
-  } 
+  }
 });
